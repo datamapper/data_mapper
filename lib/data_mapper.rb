8cae96d6d3d037a348af89fa03c5d66d3937df09
Dan Kubb <dan.kubb@gmail.com>
2009-09-30 14:39:34 -0700

    Updated version and gem dependencies

--- lib/data_mapper.rb
+++ lib/data_mapper.rb
@@ -3,6 +3,6 @@ require 'rubygems'
 DEPENDENCIES = %w[ dm-core dm-aggregates dm-constraints dm-migrations dm-serializer dm-timestamps dm-validations dm-types ]
 
 DEPENDENCIES.each do |lib|
-  gem lib, '0.10.1'
+  gem lib, '0.10.2'
   require lib
 end
